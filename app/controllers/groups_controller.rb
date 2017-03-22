b4d183731ea0658502f0f49bf46f684b5eff4ed6
ningkai <ningkaiqunar@gmail.com>
2017-03-22 21:46:42 +0800

    when created agroup, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -23,6 +23,7 @@ end
     @group = Group.new(group_params)
     @group.user = current_user
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
@@ -67,7 +68,7 @@ end
     end
 
     redirect_to group_path(@group)
-  end     
+  end
 
 
   private
